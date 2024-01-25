a68cedaf34181b1bd4196db38c7a39973607f671
Alpay Naçar <108605025+alpay9@users.noreply.github.com>
2024-01-25 12:54:55 +0300

    added randomized quicksort

---
+++ quicksort/randomized_quicksort.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template <typename T>
+int partition(vector<T>& nums, int pivot_idx, int l, int r) {
+	// put pivot into beginning
+	if (l != pivot_idx) swap(nums[l], nums[pivot_idx]);
+	T pivot = nums[l];
+
+	// partition
+	int start = l + 1, end = r;
+	while (start < end) {
+		if (nums[start] < pivot) {
+			start++;
+		}
+		else {
+			swap(nums[start], nums[end]);
+			end--;
+		}
+	}
+
+	// put pivot in between
+	if (nums[end] >= pivot) end--;
+	swap(nums[l], nums[end]);
+
+	return end;
+}
+
+template <typename T>
+void quicksort(vector<T>& nums, int l, int r) {
+	// base case is always sorted
+	if (l >= r) return;
+
+	// choose pivot randomly
+	int pivot_idx = (rand() % (r - l + 1)) + l;
+
+	int mid = partition(nums, pivot_idx, l, r);
+
+	quicksort(nums, l, mid - 1);
+	quicksort(nums, mid + 1, r);
+}
+
+template <typename T>
+void quicksort(vector<T>& nums) {
+	quicksort(nums, 0, nums.size() - 1);
+}
